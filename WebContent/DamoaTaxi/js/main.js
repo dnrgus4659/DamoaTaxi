3bf6381768489dcd8fab6a9eab28174c7bc9592e
ukhyeon <dnrgus4659@naver.com>
2019-09-24 07:58:31 +0900

    nav 수정 및 로고 이미지 수정

--- WebContent/DamoaTaxi/js/main.js
+++ WebContent/DamoaTaxi/js/main.js
@@ -9,7 +9,7 @@
    // clearTimeout(timer);
    $this.addClass('show');
    $this.find('> a').attr('aria-expanded', true);
-   // $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+   //$this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
    $this.find('.dropdown-menu').addClass('show');
  }, function(){
    var $this = $(this);
@@ -17,7 +17,7 @@
    // timer = setTimeout(function(){
      $this.removeClass('show');
      $this.find('> a').attr('aria-expanded', false);
-     // $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+     //$this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
      $this.find('.dropdown-menu').removeClass('show');
    // }, 100);
  });
